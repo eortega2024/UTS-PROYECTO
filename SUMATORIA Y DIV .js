eb8f18a9b7ababf0714c1f8f58275a8f2b907bcc
eortega2024 <eyortega@uts.edu.co>
2024-08-12 21:54:56 -0500

    Se agrega el proyecto.

---
+++ SUMATORIA Y DIV .js
@@ -0,0 +1,14 @@
+// Definir las variables
+let A = 5;
+let B = 10;
+let C = 15;
+
+// Sumar los tres valores
+let suma = A + B + C;
+
+// Calcular el promedio
+let promedio = suma / 3;
+
+// Mostrar el resultado
+console.log("La suma es: " + suma);
+console.log("El promedio es: " + promedio);
